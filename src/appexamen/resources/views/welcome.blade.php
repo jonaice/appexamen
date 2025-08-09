abc788b67e0400dfb3c97087496bf783d387a99b
Jonatan Eduardo Ramirez Lopez <jonatanrammirez@gmail.com>
2025-08-09 16:10:30 -0600

    definiendo el side bar

--- src/appexamen/resources/views/welcome.blade.php
+++ src/appexamen/resources/views/welcome.blade.php
@@ -6,9 +6,15 @@
     <title>@yield('title', 'Panel')</title>
     @vite(['resources/css/app.css','resources/js/app.js'])
   </head>
-  <body class="min-h-screen bg-gray-50 text-gray-800 dark:bg-gray-900 dark:text-gray-100">
-    <div class="flex min-h-screen">
-      @includeIf('tailadmin.partials.sidebar')
+  <body
+  x-data="{ page: 'ecommerce', 'loaded': true, 'darkMode': false, 'stickyMenu': false, 'sidebarToggle': false, 'scrollTop': false }"
+    x-init="
+         darkMode = JSON.parse(localStorage.getItem('darkMode'));
+         $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))"
+    :class="{'dark bg-gray-900': darkMode === true}"
+  >
+    <div class="flex h-screen overflow-hidden">
+      @includeIf('sidebar')
     </div>
   </body>
 </html>
