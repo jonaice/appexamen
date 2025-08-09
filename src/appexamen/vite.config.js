031849f76cb09bf469d286f2ecbdfefa909eac01
Jonatan Eduardo Ramirez Lopez <jonatanrammirez@gmail.com>
2025-08-09 14:57:19 -0600

    Copiando imágenes de la Plantilla

--- src/appexamen/vite.config.js
+++ src/appexamen/vite.config.js
@@ -3,6 +3,11 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        host: '127.0.0.1',
+        port: 5173,
+        hmr: { host: '127.0.0.1', port: 5173 },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
