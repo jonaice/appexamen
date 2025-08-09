fc88336887ecb11013c83c8173f0f69968904711
Jonatan Eduardo Ramirez Lopez <jonatanrammirez@gmail.com>
2025-08-09 15:10:11 -0600

    pegando imports de la plantilla a nuestro proyecto

--- src/appexamen/resources/js/app.js
+++ src/appexamen/resources/js/app.js
@@ -1,1 +1,16 @@
 import './bootstrap';
+import "jsvectormap/dist/jsvectormap.min.css";
+import "flatpickr/dist/flatpickr.min.css";
+import "dropzone/dist/dropzone.css";
+
+import Alpine from "alpinejs";
+import persist from "@alpinejs/persist";
+import flatpickr from "flatpickr";
+import Dropzone from "dropzone";
+
+import chart01 from "./components/charts/chart-01";
+import chart02 from "./components/charts/chart-02";
+import chart03 from "./components/charts/chart-03";
+import map01 from "./components/map-01";
+import "./components/calendar-init.js";
+import "./components/image-resize";
